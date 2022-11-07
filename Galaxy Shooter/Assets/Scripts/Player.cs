ffe88300ab80fcf609ca41541c135f0e509a343b
Daniel Martin <daniel.martin.bsas@gmail.com>
2022-11-07 22:20:32 +0100

    Player Movement

--- Galaxy Shooter/Assets/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // X+Y Movement
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        // Boundaries X
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, verticalInput, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, verticalInput, 0);
+        }
+
+        // Boundaries Y
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
     }
 }
